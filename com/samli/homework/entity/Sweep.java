76eeda99f92ed98353bea3141599642d49d5137a
sampson.pengyu.li@kingland.com <sampson.pengyu.li@kingland.com>
2023-04-16 21:29:39 +0800

    SWEEP-1 Add entity

--- com/samli/homework/entity/Sweep.java
+++ com/samli/homework/entity/Sweep.java
@@ -4,46 +4,35 @@ import java.util.*;
 
 public class Sweep {
     public static void main(String[] args) {
+        // 1. 从前端传入第一个翻开的方块的坐标，因为第一个不可能是雷。此处假设第一个翻开的是（0，1）
+        int firstHorizontal = 0;
+        int firstVertical = 1;
 
-//        List<MineSquare> mineSquares = new ArrayList<>();
-//
-//        for (int i = 0; i < 10; i++) {
-//            for (int j = 0; j < 10; j++) {
-//                MineSquare mineSquare = new MineSquare();
-//                mineSquare.setHorizontal(i);
-//                mineSquare.setVertical(j);
-//                mineSquares.add(mineSquare);
-//            }
-//        }
-
-        int fisrt = 0;
-        int first2 = 1;
-
-
-        List<Map<Integer, Integer>> a = new ArrayList<>();
+        // 2. 建立一个雷的集合
+        List<Map<Integer, Integer>> mines = new ArrayList<>();
+        // 3. 一共要生成10颗雷，count用来计算当前生成了第几颗。
         int count = 0;
+        // 3. 开始循环生成
         while (count <= 9) {
-            Random r = new Random();
-            Random r2 = new Random();
-            int i1 = r.nextInt(8); // 生成[0,8]区间的整数
-            int i2 = r2.nextInt(8); // 生成[0,8]区间的整数
-            System.out.println("======================");
-            if (i1 == fisrt && i2 == first2) {
-                System.out.println("第一颗不是雷");
+            // 3.1 生成横纵坐标的随机数对象
+            int horizontalRandom = new Random().nextInt(8); // 生成[0,8]区间的整数
+            int verticalRandom = new Random().nextInt(8); // 生成[0,8]区间的整数
+            // 3.2 判断是否是第一颗
+            if (horizontalRandom == firstHorizontal && verticalRandom == firstVertical) {
+                System.out.println("这个坐标和第一个重复啦！第一颗不可能是雷。");
             } else {
-                Map<Integer, Integer> m = new HashMap<>();
-                m.put(i1, i2);
-                if (a.contains(m)) {
-                    System.out.println("重复啦，雷的坐标为：" + i1 + "," + i2);
+                // 3.2.1 构造雷的对象
+                Map<Integer, Integer> mine = new HashMap<>();
+                mine.put(horizontalRandom, verticalRandom);
+                // 3.2.2 判断雷是否已经生成过了
+                if (mines.contains(mine)) {
+                    System.out.println("重复啦，雷的坐标为：" + horizontalRandom + "," + verticalRandom);
                     continue;
                 }
-                a.add(m);
-
+                mines.add(mine);
                 count++;
-                System.out.println("第" + count + "颗雷的坐标为：" + i1 + "," + i2);
+                System.out.println("第" + count + "颗雷的坐标为：" + horizontalRandom + "," + verticalRandom);
             }
         }
-
-
     }
 }
